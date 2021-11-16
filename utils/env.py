b3aad47de081b2df710ea0afa1f59fd89f1b8c78
Carter Abdallah <carterabdallah@gmail.com>
2021-11-15 21:57:30 -0500

    Adding Completed Project to Git Repo To put on Website

---
+++ utils/env.py
@@ -0,0 +1,25 @@
+# IAM User access configuration
+AWS_ACCESS_KEY="AKIAVMGIBNISI346QG4A"
+AWS_SECRET_ACCESS_KEY="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"
+AWS_REGION="us-east-1"
+
+# S3 Bucket for photo objects
+PHOTOGALLERY_S3_BUCKET_NAME="photobucket-abdallah-2021-ece4150-lab2"
+
+# MySQL Configuration
+RDS_DB_HOSTNAME="photogallerydb.crv0a7lxeqni.us-east-1.rds.amazonaws.com"
+RDS_DB_USERNAME='root'
+RDS_DB_PASSWORD='48849JHi'
+RDS_DB_NAME='photogallerydb'
+
+# DynamoDB Table
+DYNAMODB_TABLE='PhotoGallery-Lab2'
+USER_DYNAMODB_TABLE ='PhotoGalleryUser'
+
+###### INSERT NEW ENVIRONMENT VARIEABLES HERE ######
+
+HASH_SERIALIZER = 'secret-key-Carter'
+
+CURRENT_URL = 'http://128.61.79.237:5001/'
+EMAIL_SOURCE = 'cabdallah7@gatech.edu'
+####################################################
